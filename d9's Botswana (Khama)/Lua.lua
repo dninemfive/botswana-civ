ba086e085b46011cdfcba1f930dbe2a6bf0bcb67
dninemfive <11084010+dninemfive@users.noreply.github.com>
2019-07-23 22:14:15 -0700

    UA working, fixing UU...

--- d9's Botswana (Khama)/Lua.lua
+++ d9's Botswana (Khama)/Lua.lua
@@ -20,32 +20,33 @@ function d9BotswanaDoTurn(iplayer)
 		print("processing Botswana's turn...")
 		d9BotswanaPula(player)
 		d9BatswanaKgosi(player)
+		if 1 then print("1 equals true") end
 	end
 end
 GameEvents.PlayerDoTurn.Add(d9BotswanaDoTurn);
 --====== Trait lua ======--
 function d9BotswanaPula(player)
-	print("  pula:")
+	-- print("  pula:")
 	if player:GetNumCities() > 0 then
 		for city in player:Cities() do
-			print("    processing " .. city:GetName())
+			-- print("    processing " .. city:GetName())
 			-- count freshwater worked tiles 
 			local ct = 0
 			-- Chrisy15 credits this to Bane_
 			for i = 0, city:GetNumCityPlots() - 1, 1 do
-				print("      inside for loop: " .. i)
+				-- print("      inside for loop: " .. i)
 				local plot = city:GetCityIndexPlot(i)
 				if (plot ~= nil) and (plot:GetWorkingCity() == city) and plot:IsBeingWorked() and plot:IsFreshWater() then
 					ct = ct + 1 -- I miss ct++ :(
 				end
 			end
-			print("end for loop")
+			-- print("end for loop")
 			-- give city food dummies equal to floor(count/2)
 			city:SetNumRealBuilding(food, math.floor(ct/foodDiv))
-			print("    gave " .. city:GetName() .. math.floor(ct/foodDiv) .. " food (ct = " .. ct .. ")")
+			-- print("    gave " .. city:GetName() .. math.floor(ct/foodDiv) .. " food (ct = " .. ct .. ")")
 			-- if city is growing, give 2 happiness dummy
 			if city:FoodDifference() > 0 then
-				print("City is growing, giving happiness...")
+				-- print("City is growing, giving happiness...")
 				city:SetNumRealBuilding(happiness, growthHappiness)
 			end
 		end
@@ -55,10 +56,10 @@ end
 function d9BatswanaKgosi(player) -- not a typo, it's the adjectival form if I'm not mistaken
 	-- if player:GetCivilizationType() == botswana then -- checked in the doturn call
 	if player:HasUnitOfClassType(iGeneralClass) then
-		print("Botswana has kgosi")
+		print("  kgosi:")
 		-- iterate through all units, remove any ignoreZOC promotions
 		for unit in player:Units() do
-			pUnit:SetHasPromotion(ignoreZOC, false)
+			unit:SetHasPromotion(ignoreZOC, false)
 		end
 		-- iterate through all units, grant ZOC promotions to units stacked with or adjacent to Kgosi, heal if Kgosi in hills
 		for unit in player:Units() do			
@@ -71,7 +72,6 @@ function d9BatswanaKgosi(player) -- not a typo, it's the adjectival form if I'm 
 					if isInHills then
 						other:ChangeDamage(healAmount)
 					end
-					-- if 1 then print("1 equals true")
 				end
 			end
 		end
@@ -85,7 +85,7 @@ function LastKgosiKilled(playerID, unitID, unitType, iX, iY, bDelay)
             local pPlayer = Players[playerID]
             if (pPlayer:GetCivilizationType() == botswana) and (not pPlayer:HasUnitOfClassType(iGeneralClass)) then
                 for pUnit in pPlayer:Units() do
-                    pUnit:SetHasPromotion(ignoreZOC, false)
+					pUnit:SetHasPromotion(ignoreZOC, false)
                 end
             end
         end
